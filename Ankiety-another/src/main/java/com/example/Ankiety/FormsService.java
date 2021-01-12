07c3639e2ffcadc359ea2adaf8f0390a0d63e46f
Durill <70134706+Durill@users.noreply.github.com>
2021-01-12 10:18:47 +0100

    Add files via upload

---
+++ Ankiety-another/src/main/java/com/example/Ankiety/FormsService.java
@@ -0,0 +1,14 @@
+package com.example.Ankiety;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormsService {
+
+    @Autowired
+    FormsRepository formsRepository;
+
+
+
+}
--- Ankiety-another/src/main/java/com/example/Ankiety/MainController.java
+++ Ankiety-another/src/main/java/com/example/Ankiety/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 
     @Autowired
     private FormsRepository formsRepository;
+
     private boolean isSaved=false;
 
 
@@ -21,21 +22,22 @@ public class MainController {
     @GetMapping("/")
     public String showCreateForm(Model model){
         model.addAttribute("former", new Forms());
-        Forms forms = new Forms();
+        isSaved=false;
         return "create_form";
     }
 
     @PostMapping("/definegroup")
     public String createForm(@ModelAttribute Forms forms, Model model, String formName, String email
-    ,Integer quantity1, Integer quantity2
-            , Integer quantity3, Integer quantity4, Integer quantity5){
+    ,Integer quantity1, Integer quantity2, Integer quantity3, Integer quantity4, Integer quantity5){
         model.addAttribute("former", forms);
+
         if(!formsRepository.checkIfExist(formName, email) & isSaved==false){
             formsRepository.save(forms);
             isSaved=true;
         }
         model.addAttribute("name", forms.getFormName());
         model.addAttribute("email", forms.getEmail());
+
         Integer findedForm = formsRepository.findFormByName(forms.getFormName(), forms.getEmail());
         model.addAttribute("choices", findedForm);
         String answerOne = formsRepository.findAnswers(forms.getFormName(), forms.getEmail());
@@ -43,12 +45,14 @@ public class MainController {
                 .map(String::trim)
                 .collect(Collectors.toList());
         model.addAttribute("splitanswer", splitStr);
+
         Long showId = formsRepository.findId(formName, email);
         String quantities = formsRepository.showQuantities(formName, email, showId);
         List<String> splitQnt = Arrays.stream(quantities.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
         model.addAttribute("splitQuantities", splitQnt);
+
         if(formsRepository.checkIfExist(formName, email) & isSaved==true){
             formsRepository.updateQuantity( formName, email, quantity1, quantity2, quantity3, quantity4, quantity5);
         }
@@ -57,40 +61,32 @@ public class MainController {
 
 
     @PostMapping("/showanswers")
-    public String showAnswers(@ModelAttribute Forms forms, Model model
-    , String formName, String email, Integer quantity1, Integer quantity2
-            , Integer quantity3, Integer quantity4, Integer quantity5){
+    public String showAnswers(@ModelAttribute Forms forms, Model model, String formName, String email
+            , Integer quantity1, Integer quantity2, Integer quantity3, Integer quantity4, Integer quantity5){
         model.addAttribute("former", forms);
+
         formsRepository.updateQuantity( formName, email, quantity1, quantity2, quantity3, quantity4, quantity5);
         String answerOne = formsRepository.findAnswers(forms.getFormName(), forms.getEmail());
+
         model.addAttribute("former", forms);
         model.addAttribute("name", forms.getFormName());
         model.addAttribute("email", forms.getEmail());
+
         Long showId = formsRepository.findId(formName, email);
+
         String quantities = formsRepository.showQuantities(formName, email,showId);
         List<String> splitQnt = Arrays.stream(quantities.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
         model.addAttribute("splitQuantities", splitQnt);
+
         List<String> splitStr = Arrays.stream(answerOne.split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
         model.addAttribute("splitanswer", splitStr);
+
         return "show_answers";
 
-    }
-
-    @GetMapping("/stopvoting")
-    public String stopVoting(){
-        return "stop_voting";
-    }
-
-    @RequestMapping("/formsaved")
-    @ResponseBody
-    public String queryStringMapping(
-            @RequestParam String param1,
-            @RequestParam String param2) {
-        return String.format("Otrzymane wartości: param1=%s, param2=%s", param1, param2);
     }
 
 
